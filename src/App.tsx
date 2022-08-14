c7f38264782db3de7276f60adf6510a48d7eb41b
astream <positive.thought1009@gmail.com>
2022-08-14 19:35:04 +0900

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { ListItem } from './components/ListItem';
 
-function App() {
+type User = {
+  id: number;
+  name: string;
+  age: number;
+  personalColor: string;
+};
+
+
+export const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+useEffect(() => {
+  axios.get<User[]>('https://example.com/users').then((res) => {
+    setUsers(res.data);
+  })
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map(user => (
+        <ListItem id={user.id} name={user.name} age={user.age} personalColor={user.personalColor} />
+      ))}
     </div>
   );
 }
-
-
-export default App;
